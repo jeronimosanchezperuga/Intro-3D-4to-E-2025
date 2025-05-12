0468eff85a4a061f6c219eb247faf39eb8ac9083
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2025-05-12 12:11:54 -0300

    Random Cloner terminado

---
+++ Assets/RandomCloner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCloner : MonoBehaviour
+{
+    public GameObject prefab1;
+    public GameObject prefab2;
+    public GameObject prefab3;
+
+    public int randomNumber;
+    public int min = 0;
+    public int max = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void CloneRandom()
+    {
+        randomNumber = Random.Range(min, max + 1);
+        if (randomNumber == 0)
+        {
+            Instantiate(prefab1);
+        }
+        else if (randomNumber == 1)
+        {
+            Instantiate(prefab2);
+        }
+        else
+        {
+            Instantiate(prefab3);
+        }
+    }
+}
